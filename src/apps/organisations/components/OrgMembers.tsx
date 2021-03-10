e472af812806150c31a287d5bb4849c4af6a5bd1
Irenyak1 <inyakate@gmail.com>
2021-03-10 18:13:24 +0300

    OCLOMRS-936 Fixes overflowing fieldsets in details page for sources and trancated inputs for dictionaries and sources in view and edit mode
    OCLOMRS-936:Fixes overlapping fieldsets and hidden inputs for sources and dictionaries

--- src/apps/organisations/components/OrgMembers.tsx
+++ src/apps/organisations/components/OrgMembers.tsx
@@ -92,7 +92,7 @@ const OrganisationMembers: React.FC<Props> = ({ members, orgUrl, addMember, load
                 </IconButton>
               }
             >
-              {(addError &&`Adding: ${addError}`) || (deleteError && `Deleting: ${deleteError}`)}
+           {addError ?`Adding:${addError}` : deleteError ? `Deleting:${deleteError}` : 'Some Error occured'}
             </Alert>
           </Collapse>
             <List>
